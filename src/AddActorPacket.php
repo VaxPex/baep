5355cd648007c7346c72e6ba976f0a6364449de4
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2022-07-13 17:10:05 +0200

    1.19.10

--- src/AddActorPacket.php
+++ src/AddActorPacket.php
@@ -159,6 +159,7 @@ class AddActorPacket extends DataPacket{
 	public $yaw = 0.0;
 	/** @var float */
 	public $headYaw = 0.0;
+	public float $bodyYaw = 0.0;
 
 	/** @var Attribute[] */
 	public $attributes = [];
@@ -179,6 +180,7 @@ class AddActorPacket extends DataPacket{
 		$this->pitch = $this->getLFloat();
 		$this->yaw = $this->getLFloat();
 		$this->headYaw = $this->getLFloat();
+		$this->bodyYaw = $this->getLFloat();
 
 		$attrCount = $this->getUnsignedVarInt();
 		for($i = 0; $i < $attrCount; ++$i){
@@ -214,6 +216,7 @@ class AddActorPacket extends DataPacket{
 		$this->putLFloat($this->pitch);
 		$this->putLFloat($this->yaw);
 		$this->putLFloat($this->headYaw);
+		$this->putLFloat($this->bodyYaw);
 
 		$this->putUnsignedVarInt(count($this->attributes));
 		foreach($this->attributes as $attribute){
